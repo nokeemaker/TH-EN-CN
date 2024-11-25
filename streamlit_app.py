3c901b6b81429957240d84f8cf10f8c97e1bd2a8
nokeemaker <nokeework@gmail.com>
2024-11-25 13:04:18 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,15 @@ if user_api := st.text_input("Your API key: ", type="password"):
 
     if submitted:
         prompt = """The input needs to be in Thai, English or Chinese.
+                Your goal is to help learners of these three languages develop understandings of the three at the same time, so be comprehensive in extracting words.
                 Create only a dictionary designed to be displayed by a Pandas dataframe(table) after performing the following actions: 
                 - Make sure to include three languages.
                 - Make sure to includes pinyin for Chinese in brackets immediately after the Chinese scripts.
-                1. Extract nouns, verbs, and adjectives "literally" from the text, limiting at the total of 50, based on the detected language, since the text is not always in Thai.
+                1. Extract essential nouns, verbs, and adjectives "literally" from the text, limiting at the total of 50, based on the detected language, since the text is not always in Thai.
                 2. Create the first table for each word class, in column 2 with a extracted word and, in columns 3 and 4 with the word 
                 translations of the other languages. The columns 5 and 6 give a sentence examples in each language. 
                 After that, in the last two rows, make five sentences in each language using all the extracted, one row for each language's five sentences.
+                
                 Note!! Make sure to follow this format, every row must be of the same length:
                   data = {
                     "Word Class": ["Noun", ..., "Verb", ..., "Adjective", ...],
